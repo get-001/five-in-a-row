f4b51ef06c4a8f10de7f296b1eff6a119e3e1b89
get-001 <1093336886@qq.com>
2020-03-23 20:10:38 +0800

    build

---
+++ build/precache-manifest.28418dc31b62304adecbd1215e8f4df4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e5d45d6b7df3f002322f5c6cfa2d08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "481ce84acfae78043ce9",
+    "url": "/static/css/main.49b8ffba.chunk.css"
+  },
+  {
+    "revision": "f762ee80bcef52dc598c",
+    "url": "/static/js/2.d05ea88b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d05ea88b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "481ce84acfae78043ce9",
+    "url": "/static/js/main.62f54311.chunk.js"
+  },
+  {
+    "revision": "ca7f4c525ab2d54bed25",
+    "url": "/static/js/runtime-main.996845be.js"
+  },
+  {
+    "revision": "75a82f4553bb28bb4feb7a7edf973da4",
+    "url": "/static/media/hover.75a82f45.png"
+  }
+]);
